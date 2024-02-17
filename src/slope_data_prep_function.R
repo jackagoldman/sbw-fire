d2231efebbb8937ce314b83ec91d584f65f82948
jackagoldman <jandrewgoldman@gmail.com>
2024-02-16 19:54:57 -0500

    read data to slope

--- src/slope_data_prep_function.R
+++ src/slope_data_prep_function.R
@@ -1,5 +1,12 @@
 # Data prep for slope function
 
+
+getData <- function(file){
+  readr::read_csv(file)
+}
+
+
+
 #' slope data prep
 #'
 #' @param data dataframe with unique id, fire_name and nbr1-10 variables. One row per fire.
@@ -9,14 +16,15 @@
 #'
 #' @examples data <- slopePrep(data)
 
-slopePrep <- function(data){
+slopePrep <- function(x){
   
-  require(tidyverse)
-  
-  x <- data |> 
-    select(c(id, fire_name, nbr1, nbr2, nbr3, nbr4, nbr5, nbr6, nbr7, nbr8, nbr9,nbr10)) |> 
-    pivot_longer(!c(fire_name, id), names_to = "Recovery_Time", values_to = "nbr" ) |> 
-    mutate(recovery_interval = case_when(Recovery_Time == "nbr1" ~ 1, 
+    x <- dplyr::select(x, c(id, fire_name, nbr1, nbr2, nbr3, nbr4, 
+                               nbr5, nbr6, nbr7, nbr8, nbr9,nbr10))
+    
+    x <- tidyr::pivot_longer(x, !c(fire_name, id), names_to = "Recovery_Time", values_to = "nbr" )  
+    
+    
+    x <- dplyr::mutate(x, recovery_interval = case_when(Recovery_Time == "nbr1" ~ 1, 
                                          Recovery_Time == "nbr2" ~ 1,
                                          Recovery_Time == "nbr3" ~ 1, 
                                          Recovery_Time == "nbr4" ~ 1, 
@@ -28,8 +36,7 @@ slopePrep <- function(data){
                                          Recovery_Time == "nbr10" ~ 2))
   
   #scale nbr
-  x <- x |> 
-    mutate(nbr = (nbr*10^3))
+  x <- dplyr::mutate(x, nbr = (nbr*10^3))
   
   # make sure all columns are lower case
   names(x) <- tolower(names(x))

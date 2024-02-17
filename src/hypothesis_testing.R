d0090bed09c15c2665bda7db566618888121e154
jackagoldman <jandrewgoldman@gmail.com>
2024-02-17 11:59:45 -0500

    slope ttest

--- src/hypothesis_testing.R
+++ src/hypothesis_testing.R
@@ -52,17 +52,69 @@ severity_ttest <-  function(df){
   results <- rbind(median, extreme, cv)
   
   # round the results
-  results <- dplyr::mutate(results, `P-value` = case_when(`P-value` < 0.0001 ~ 0.0001,
-                                                                   TRUE ~ round(as.numeric(results$`P-value`), digits = 3)))
-
+  results <- dplyr::mutate(results, `P-value` = as.numeric(`P-value`))
+  results <- dplyr::mutate(results, `P-value` = formatC(as.numeric(`P-value`),
+                                                        format = "f", digits = 4))
+  
+  results <- dplyr::mutate(results, `P-value` = case_when(`P-value` < 0.0001 ~ "<0.0001",
+                                                          TRUE ~ formatC(as.numeric(`P-value`),
+                                                                         format = "f", digits = 3)))
   return(results)
 }
 
-recovery_ttest <- function(data){
+recovery_ttest <- function(df){
+  
+  # make sure function does not use scientific notation 
+  op <- options(scipen=999)
+  on.exit(options(op), add = TRUE)
+  
+  # run paired t-test
+  sens10 <- t.test(df$defoliated, df$sens10, paired = TRUE) 
+  sens1 <- t.test(df$defoliated, df$sens1, paired = TRUE) 
+  sens2 <- t.test(df$defoliated, df$sens2, paired = TRUE) 
+  
+  # extract values
+  s10_t <- sens10$statistic[[1]]
+  s10_p <- sens10$p.value
+  s10_df <- sens10$parameter[[1]]
+  
+  s1_t <- sens1$statistic[[1]]
+  s1_p <- sens1$p.value
+  s1_df <- sens1$parameter[[1]]
+  
+  s2_t <- sens2$statistic[[1]]
+  s2_p <- sens2$p.value
+  s2_df <- sens2$parameter[[1]]
+  
+  #make df
+  cols <- c("Test", "t-value", "df", "P-value")
+  sn10 <- cbind("10yr post-fire", s10_t, s10_df, as.numeric(s10_p))
+  sn10 <- as.data.frame(sn10)
+  colnames(sn10) <- cols
   
   
+  sn1 <- cbind("1-5yr post-fire", s1_t, s1_df, as.numeric(s1_p))
+  sn1 <- as.data.frame(sn1)
+  colnames(sn1) <- cols
   
   
+  sn2 <- cbind("6-10yr post-fire", s2_t, s2_df, as.numeric(s2_p))
+  sn2 <- as.data.frame(sn2)
+  colnames(sn2) <- cols
+  
+  # rbind
+  results <- rbind(sn10, sn1, sn2)
+  
+  # round the results
+  results <- dplyr::mutate(results, `P-value` = as.numeric(`P-value`))
+  results <- dplyr::mutate(results, `P-value` = formatC(as.numeric(`P-value`),
+                                                       format = "f", digits = 4))
+  
+  results <- dplyr::mutate(results, `P-value` = case_when(`P-value` < 0.0001 ~ "<0.0001",
+                                                          TRUE ~ formatC(as.numeric(`P-value`),
+                                                                         format = "f", digits = 3)))
+  
+  return(results)
   
   
 }
